a52b28e1737a864fc47356f9e072c40f45b8e5fa
zhucchinizz <145158313+zhucchinizz@users.noreply.github.com>
2023-10-31 15:34:59 -0400

    Update script.js

--- script.js
+++ script.js
@@ -1,1 +1,10 @@
+function initilize(){
+  power = document.getElementById(pVal);
+  pVar = 0;
+  speed = document.getElementById(sVal);
+  sVar = 0;
+  
+  StructuralIntegritiy = document.getElementById(siVal);
+  siVar = 0;
+}
 plus(String)
